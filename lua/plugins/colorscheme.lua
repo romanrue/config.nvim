fef647314978788e404e59e6cbe589eee9b24b4a
Roman Ruttimann <roman@localhost.localdomain>
2024-10-18 08:01:08 +0200

    feat: Change color theme
    Deselects tokyonight, the default color scheme of LazyVim, and loads 2
    new scheme packets:
    
    - catppuccin
    - everforest
    
    -> everforest is selected as new theme in `core.rs`

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- A dark light Neovim theme written in lua ported from the VS Code TokyoNight
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- Port of Catppuccin, a community driven pastel theme
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- A green based color scheme; it's designed to be warm to protect developers' eyes.
+  { "neanias/everforest-nvim", version = false, priority = 1000, opt = {} },
+}
